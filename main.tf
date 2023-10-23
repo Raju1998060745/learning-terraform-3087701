39e2841b8e173a8b2f50ed6cec7ef76acd150c91
Raju1998060745 <99238367+Raju1998060745@users.noreply.github.com>
2023-10-23 19:35:44 -0400

    removed resources

--- main.tf
+++ main.tf
@@ -35,36 +35,6 @@ resource "aws_security_group" "tester" {
   vpc_id       = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "tester_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.tester.id
-
-}
-
-resource "aws_security_group_rule" "tester_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.tester.id
-
-}
-
-resource "aws_security_group_rule" "allow_everything" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.tester.id
-
-}
-
 
 module "security-group" {
   source       = "terraform-aws-modules/security-group/aws"
