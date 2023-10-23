a892ab38f62ba0ffc4c4a3c09e48b1b6e2be63cb
Raju1998060745 <99238367+Raju1998060745@users.noreply.github.com>
2023-10-23 18:44:09 -0400

    adding security groups

--- main.tf
+++ main.tf
@@ -14,11 +14,52 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default"{
+  default=true
+}
+
 resource "aws_instance" "tester" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.tester.id ]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "tester" {
+  name         = tester
+  description  = "security group for learning terraform that allows http and https"
+  vpc_id       = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "tester_http_in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.tester.id
+
+}
+
+resource "aws_security_group_rule" "tester_http_in" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.tester.id
+
+}
+
+resource "aws_security_group_rule" "tester_http_in" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.tester.id
+
 }
